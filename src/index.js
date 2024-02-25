e84648597ae940ae7090f9fa8310d6fb01b70113
FredrikAndreHansen <fredrikahansen90@gmail.com>
2024-02-25 15:45:06 +0100

    Added a todo list of the project in index.js

--- src/index.js
+++ src/index.js
@@ -22,3 +22,33 @@ if (HAS_INTERNET_CONNECTION()) {
 else {
     initOffline();
 }
+
+// Todo
+
+// ## Users menu
+// Invite users to teams via the User menu
+
+// ##Teams menu
+// Settings: 
+    // Rename team
+    // Set team rules (allow to add users, allow to remove users, allowed to schedule meetings)
+    // Delete team
+// Kick pending invites & members
+
+// ## Schedule metting
+// After scheduling a meeting you can select to keep the meeting or discard it (If kept it will then show as a upcoming meeting in the notification menu, it will automatically dissapear when the date past the meeting end time!)
+// When scheduling:
+    // Date, start and end time of meeting
+    // Select team
+    // Get a list of all users (so that you can discard users for the meeting(in case someone is sick!))
+    // Pending meeting result page, it will also appear in the notification menu (for all affected users!)
+
+// ## Main menu
+// Have a notification when declining a meeting
+// Settings in main menu
+    // Change username
+    // Change company name
+    // Change password
+    // Change email
+    // Always signed in or sign out when removing the app
+    // Delete account, inform that all user data will be removed from the server
