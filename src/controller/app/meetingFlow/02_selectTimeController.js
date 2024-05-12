7da7e2714acea0f43036845c68c3e67d0522d31c
FredrikAndreHansen <fredrikahansen90@gmail.com>
2024-05-12 18:16:32 +0200

    Added the function to add and delete time for all dates

--- src/controller/app/meetingFlow/02_selectTimeController.js
+++ src/controller/app/meetingFlow/02_selectTimeController.js
@@ -1,14 +1,54 @@
 export class SelectTimeController {
     
-    constructor(helpers, views) {
+    constructor(helpers, views, selectTimeModel) {
         this.helpers = helpers;
         this.views = views;
+        this.selectTimeModel = selectTimeModel;
     }
 
     setView(meetingData) {
+        this.#generateOutput(meetingData);
+
+        this.#setTimeEventListener(meetingData);
+    }
+
+    #generateOutput(meetingData) {
         this.helpers.SET_INNER_HTML_VALUE({set: this.views.viewDOMElement, to: this.views.selectTimeView(meetingData)});
 
-        this.#goBack(meetingData)
+        this.selectTimeModel.outputSelectedTimesDate(meetingData);
+    }
+
+    #setTimeEventListener(meetingData) {
+        for(let i = 0; i < meetingData.dates.length; i++) {
+            const id = meetingData.dates[i].date;
+            const timeDomElement = document.querySelector('.time-' + id);
+
+            document.querySelector('#id' + id).addEventListener('click', () => {
+                this.selectTimeModel.addTime(id, timeDomElement.value, meetingData);
+
+                this.#setTimeEventListener(meetingData);
+            });
+        }
+        this.#removeTime(meetingData);
+
+        this.#goBack(meetingData);
+    }
+
+    #removeTime(meetingData) {
+        for (let i = 0; i < meetingData.dates.length; i++) {
+            for (let j = 0; j < meetingData.dates[i].time.length; j++) {
+                const time = meetingData.dates[i].time[j];
+                const timeTrim = this.helpers.REMOVE_SEMICOLON(time);
+                const date = meetingData.dates[i].date;
+                const individualTimeDomElement = document.querySelector('#id' + date + "-" + timeTrim);
+
+                individualTimeDomElement.addEventListener('click', () => {
+                    this.selectTimeModel.removeTime(date, time, meetingData);
+
+                    this.#setTimeEventListener(meetingData);
+                });
+            }
+        }
     }
 
     #goBack(meetingData) {
--- src/model/app/meetingModel.js
+++ src/model/app/meetingModel.js
@@ -2,4 +2,42 @@ export class MeetingModel {
 
     constructor() {}
 
+    canProceed(ifUserCanProceed) {
+        const proceedDOMElement = document.querySelector('#proceed');
+
+        if (ifUserCanProceed === true) {
+            if (proceedDOMElement.classList.contains('none')){
+                proceedDOMElement.classList.remove('none');
+            }
+        } else {
+            if (!proceedDOMElement.classList.contains('none')) {
+                proceedDOMElement.classList.add('none');
+            }
+        }
+    }
+
+    formatDateOutput(date) {
+        const year = this.setDateVariables(date).year;
+        const month =  this.setDateVariables(date).month;
+        const day =  this.setDateVariables(date).day;
+        const allMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const currentMonth = allMonths[month-1];
+
+        return currentMonth + ' ' + day + ', ' + year;
+    }
+
+    setDateVariables(date) {
+        const dateArray = date.split("-");
+
+        const year = dateArray[0];
+        const month = dateArray[1];
+        const day = dateArray[2];
+
+        return {
+            year: year,
+            month: month,
+            day: day
+        };
+    }
+
 }
